797d13871a599e63743276b34903335068a05014
print-Eruki <palejorniel@gmail.com>
2024-12-21 23:00:46 -0400

    Added database Config using godotenv

---
+++ configs/config.go
@@ -0,0 +1,34 @@
+package config
+
+import (
+	"log"
+
+	"github.com/caarlos0/env/v6"
+	"github.com/joho/godotenv"
+)
+
+// Config holds the credentials to establish a connection with the db
+type Config struct {
+	DbName   string `env:"DBNAME,required"`
+	User     string `env:"USER,required"`
+	Password string `env:"PASSWORD,required"`
+	Host     string `env:"HOST,required"`
+	Port     string `env:"PORT,required"`
+}
+
+// Loads the env and returns a db configuration
+func LoadConfig() (*Config, error) {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("Error loading .env: %s", err)
+	}
+
+	cfg := &Config{}
+	err = env.Parse(cfg)
+	if err != nil {
+		log.Fatalf("Error parsing env to Config struct: %s", err)
+		return nil, err
+	}
+
+	return cfg, nil
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/print-Eruki/CIIC4060-chatbot
 
 go 1.23.4
+
+require (
+	github.com/caarlos0/env/v6 v6.10.1 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+)
